29566e60b279b89539a1a5be212b2bd6b947cb3a
Bence A. Tóth <tothab@gmail.com>
2020-10-21 21:00:12 +0200

    Added color code to new infecations

--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 const input = document.getElementById("country");
 const btn = document.getElementById("btn-country");
 
+const getCountryColor = ({newInfections, population}) => {
+  const infectionRate = newInfections * 1000000 / population
+  // New infections per 1M people:
+    // Green 1-71
+    // Yellow 72-143
+    // Orange 144-214
+    // Red 215-286
+    // Dark red 287+
+  if (infectionRate <= 71) {
+    return '#007516';
+  }
+  if (infectionRate <= 143) {
+    return '#6d5607';
+  }
+  if (infectionRate <= 214) {
+    return '#8c5340';
+  }
+  if (infectionRate <= 286) {
+    return '#b72e2e';
+  }
+  return '#a50000';
+}
+
 function fetchData(url) {
   document.getElementById("covid-data").innerHTML = "Loading data";
   return fetch(url).then((response) => {
@@ -29,57 +52,60 @@ function renderData(data) {
   resetView();
   const covidData = data.response[0];
   if (covidData) {
-  const covidDataHtml = `
-    <h1>${covidData.country}</h1>
-    <h3>${covidData.day}</h3>
-    <table class="dataTable">
-      <tbody>
-        <tr>
-          <th>New cases</th>
-          <td>${covidData.cases.new}</td>
-        </tr>
-        <tr>
-          <th>New deaths</th>
-          <td>${covidData.deaths.new}</td>
-        </tr>
-        <tr>
-          <th>Critical cases</th>
-          <td>${covidData.cases.critical}</td>
-        </tr>
-        <tr>
-          <th>Active cases</th>
-          <td>${covidData.cases.active}</td>
-        </tr>
-        <tr>
-          <th>Recovered cases</th>
-          <td>${covidData.cases.recovered}</td>
-        </tr>
-        <tr>
-          <th>Total deaths</th>
-          <td>${covidData.deaths.total}</td>
-        </tr>
-        <tr>
-          <th>Population</th>
-          <td>${covidData.population}</td>
-        </tr>
-      </tbody>
-    </table>
-  `;
-  document.querySelector("#covid-data").innerHTML = covidDataHtml;
- 
-  const countryHtml = `
-    <div style="width: 100%">
-      <iframe width="500" height="300" src="https://maps.google.com/maps?q=${covidData.country}&t=&z=4&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-    </div>
-  `
-  document.querySelector("#country-map").innerHTML = countryHtml;
-}else {
-  const errorHtml = `
-    <h1>Wrong country name</h1>
-  `;
-  document.querySelector("#main").innerHTML = errorHtml;
-}
+    const countryColorCode = getCountryColor({
+      newInfections: Number(covidData.cases.new),
+      population: covidData.population
+    })
+    const covidDataHtml = `
+      <h1>${covidData.country}</h1>
+      <h3>${covidData.day}</h3>
+      <table class="dataTable">
+        <tbody>
+          <tr>
+            <th>New cases</th>
+            <td style="color: ${countryColorCode}">${covidData.cases.new}</td>
+          </tr>
+          <tr>
+            <th>New deaths</th>
+            <td>${covidData.deaths.new}</td>
+          </tr>
+          <tr>
+            <th>Critical cases</th>
+            <td>${covidData.cases.critical}</td>
+          </tr>
+          <tr>
+            <th>Active cases</th>
+            <td>${covidData.cases.active}</td>
+          </tr>
+          <tr>
+            <th>Recovered cases</th>
+            <td>${covidData.cases.recovered}</td>
+          </tr>
+          <tr>
+            <th>Total deaths</th>
+            <td>${covidData.deaths.total}</td>
+          </tr>
+          <tr>
+            <th>Population</th>
+            <td>${covidData.population}</td>
+          </tr>
+        </tbody>
+      </table>
+    `;
+    document.querySelector("#covid-data").innerHTML = covidDataHtml;
 
+    const countryHtml = `
+      <div style="width: 100%">
+        <iframe width="500" height="300" src="https://maps.google.com/maps?q=${covidData.country}&t=&z=4&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      </div>
+    `
+    document.querySelector("#country-map").innerHTML = countryHtml;
+  }else {
+    const errorHtml = `
+      <h1>Wrong country name</h1>
+    `;
+    document.querySelector("#main").innerHTML = errorHtml;
+  }
 }
 
 async function getTodaysCovidData(country) {
